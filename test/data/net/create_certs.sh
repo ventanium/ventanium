02b7e8516df32e49a79e47a8a59f78e3b44dc58f
Matthias Benkendorf <mbe@ventanium.org>
2020-08-03 23:27:45 +0200

    Test certificate renewed

--- test/data/net/create_certs.sh
+++ test/data/net/create_certs.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -nodes
+openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 9125 -nodes -subj '/O=Ventanium/OU=Dev/CN=ventanium.test'
